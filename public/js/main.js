25cb5494126d2ef4e40b259fbd761fc46275d40a
KingCosmos01 <floatyard123@gmail.com>
2022-09-15 18:34:25 +0100

    Add Search

--- public/js/main.js
+++ public/js/main.js
@@ -16,3 +16,39 @@ sellerBtn.addEventListener('click', ()=> {
     sellerBtn.classList.add('active');
 
 })
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+const container = document.getElementById('map').innerText = map;
+
+const exploreBtn = document.getElementById('exploreBtn');
+
+exploreBtn.onclick = (e) => {
+    e.preventDefault();
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', '../controllers/location.php', true);
+
+    xhr.onreadystatechange = ()=> {
+        if(xhr.status === 200)
+        {
+            console.log(xhr.responseText);
+        }
+        else 
+        {
+            console.log(xhr.responseText);
+        }
+    }
+
+    xhr.send();
+
+}
